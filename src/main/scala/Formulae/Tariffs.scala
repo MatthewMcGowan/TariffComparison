e533450c49f9c7ee63408bcca98c175d54ece439
Matthew McGowan <MatthewMcGowan@users.noreply.github.com>
2017-05-20 19:48:24 +0100

    Added package for library functions

---
+++ src/main/scala/Formulae/Tariffs.scala
@@ -0,0 +1,14 @@
+package Formulae
+
+/**
+  * Created by Matt on 20/05/2017.
+  */
+class Tariffs {
+  // Seems trivial, but perhaps it could be (x / 30) * 365. Better to be explicitly defined.
+
+  /** Calculates the annual sum of the monthly standing charge.
+    * This could throw an arithmetic exception, with extremely large values, but we currently choose to assume these
+    * will not occur.
+    */
+  def annualStandingCharge(monthlyStandingCharge: BigDecimal): BigDecimal = 12 * monthlyStandingCharge
+}
---
+++ src/main/scala/Formulae/Tax.scala
@@ -0,0 +1,9 @@
+package Formulae
+
+/**
+  * Created by Matt on 20/05/2017.
+  */
+class Tax {
+  /** Calculates the Gross Price from the Net Price and VAT rate */
+  def grossPrice(netPrice: BigDecimal, vatRate: BigDecimal): BigDecimal = (1 + vatRate) * netPrice
+}
