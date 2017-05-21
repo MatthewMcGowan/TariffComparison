c61b9b453999cc91cfd3143a2704573a8f1fb081
Matthew McGowan <MatthewMcGowan@users.noreply.github.com>
2017-05-21 19:10:32 +0100

    All spec tests passing (Cost Calculator implementation).

--- src/main/scala/commands/CostCalculator.scala
+++ src/main/scala/commands/CostCalculator.scala
@@ -7,8 +7,34 @@ import models.Tariff
   */
 class CostCalculator {
   def costs(tariffs: Iterable[Tariff], powerUsage: Option[kWh], gasUsage: Option[kWh], vatRate: Percent):
-  Iterable[(String, BigDecimal)] = {
-    ???
+  Seq[(String, BigDecimal)] = {
+    def calc(t: Tariff): Option[(String, BigDecimal)] = {
+      val standingCharge = 12 * t.standingCharge
+
+      def energyCost(usage: Option[Int], rate: Option[BigDecimal]): Option[BigDecimal] = {
+        // If used but no rate, not an applicable tariff
+        if (usage.isDefined && rate.isEmpty)
+          None
+        // Otherwise, calculate
+        else
+          Some(
+            (for(x <- usage; y <- rate) yield (x * y) + standingCharge)
+              .getOrElse(0)) // Set to zero if no usage
+      }
+
+      def addVat(preVatCost: BigDecimal) = preVatCost * (1 + (vatRate / 100))
+
+      val pCost = energyCost(powerUsage, t.rates.power).map(addVat)
+      val gCost = energyCost(gasUsage, t.rates.gas).map(addVat)
+
+      (for(x <- pCost; y <- gCost) yield x + y)
+        .map((t.tariff, _))
+    }
+
+    tariffs
+      .flatMap(calc)
+      .toSeq // An ordered collection is not required (Iterable), but we do return one (Seq).
+      .sortBy(x => x._2)
   }
 
   type kWh = Int
